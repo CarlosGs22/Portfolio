173fa3671802de4d279d37532a4fd9baea9e2f5e
CarlosGs22 <carlosgs.trejo@gmail.com>
2022-02-08 15:36:55 -0600

    Update sendMail.php

--- sendMail.php
+++ sendMail.php
@@ -1,17 +1,36 @@
-<?php
+<?php 
 
 
-$headers =  'MIME-Version: 1.0' . "\r\n"; 
-$headers .= 'From: Your name <carlosgs.trejo@gmail.com>' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+require 'PHPMailer/PHPMailerAutoload.php';
 
-$to = "gustavo.trejo@uteq.edu.mx";
-$subject = "OK";
-$body = "OK";
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+// Set PHPMailer to use the sendmail transport
+$mail->isSMTP();
+$mail->Host = 'smtp.hostinger.com';              // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'carlosgs.trejo@gmail.com';                 // SMTP username
+$mail->Password = 'CGTB2623944733';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+
+//Set who the message is to be sent from
+$mail->setFrom('abc1@gmail.com', 'First Last');
+//Set an alternative reply-to address
+
+//Set who the message is to be sent to
+$mail->addAddress('abc@gmail.com', 'Shehan');
+//Set the subject line
+$mail->Subject = 'Test Mail';
+
+$mail->Body = 'This is Test Mail';
 
 
-mail($to, $subject, $body, $headers);
-
-
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+} 
 
 ?>
